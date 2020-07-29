90c86f38bea092c6dc03655c4d2c90c77b32e67a
VarmaSANJAY <sanjay.varma@relevacelab.com>
2020-07-29 23:02:53 +0530

    Added client side

--- app.js
+++ app.js
@@ -17,11 +17,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './client/dist')));
 
-app.use('/', indexRouter);
+
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/*', function (req, res) { //jshint ignore:line
+  res.sendFile(path.join(__dirname,'./client/dist', "index.html"));
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
